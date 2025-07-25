b0bfe1b00dae2b62240f17054ee6c18bf99457f5
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-07-25 19:34:00 +0200

    Run migration for devise changes at user model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_14_081216) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_14_111154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,7 +47,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_14_081216) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "measurements", "series"
