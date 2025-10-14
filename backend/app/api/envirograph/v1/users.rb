a335398de34a1d85f7ca26b6c37fda375826e01f
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-10-14 20:18:14 +0200

    implement endpoint to register new user

--- backend/app/api/envirograph/v1/users.rb
+++ backend/app/api/envirograph/v1/users.rb
@@ -26,6 +26,38 @@ module Envirograph
             user: UserSerializer.new(user).serializable_hash[:data]
           }
         end
+
+        desc "Register user" do
+          success code: 200,
+                  model: UserSerializer,
+                  message: "Returns JWT token and user data after registration"
+        end
+        params do
+          requires :first_name, type: String
+          requires :last_name, type: String
+          requires :email, type: String, desc: "User email"
+          requires :password, type: String, desc: "User password"
+          requires :password_confirmation, type: String, desc: "Password confirmation"
+        end
+        post :register do
+          user = User.new(
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            email: params[:email],
+            password: params[:password],
+            password_confirmation: params[:password_confirmation]
+          )
+          if user.save
+            token = JWT.encode({ user_id: user.id }, Rails.application.credentials.secret_key_base)
+            status 201
+            {
+              token: token,
+              user: UserSerializer.new(user).serializable_hash[:data]
+            }
+          else
+            raise Envirograph::V1::Base::ApiException.new(user.errors.full_messages.join(", "), 422)
+          end
+        end
       end
     end
   end
