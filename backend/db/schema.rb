047785203fbc1b25a1ab717fdd4e8f532f1217ba
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-10-14 20:22:22 +0200

    remove redundant column password digest form users table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_14_111154) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_14_182101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_14_111154) do
     t.string "first_name"
     t.string "last_name"
     t.string "email", null: false
-    t.string "password_digest"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
