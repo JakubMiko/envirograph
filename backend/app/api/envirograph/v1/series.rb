fcef83adcfb3b7a0718417b778c5d2ee008154e6
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-10-16 16:23:59 +0200

    define an endpoint for getting details of a single series

--- backend/app/api/envirograph/v1/series.rb
+++ backend/app/api/envirograph/v1/series.rb
@@ -15,6 +15,20 @@ module Envirograph
         end
       end
 
+      desc "Get details of a single series" do
+        success code: 200,
+                entity: Envirograph::V1::Entities::SeriesSerializer,
+                message: "Returns details of a single series"
+      end
+      params do
+        requires :id, type: Integer, desc: "ID of the series"
+      end
+      get ':id' do
+        series = Series.find_by(id: params[:id])
+        raise ApiException.new("Series not found", 404) unless series
+        SeriesSerializer.new(series).serializable_hash
+      end
+
       # To-do implement the following endpoints
       # GET /api/v1/series/:id – szczegóły jednej serii (publicznie)
       # POST /api/v1/series – utworzenie nowej serii (admin)
