12e32eb0735edc8be45b66c8741f6ce15cc44c7b
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-10-18 00:14:52 +0200

    add bootstrap-react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import SeriesList from "./SeriesList";
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -11,14 +12,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Button variant="primary" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
+        </Button>
       </header>
       <div>
         <h1>EnviroGraph</h1>
